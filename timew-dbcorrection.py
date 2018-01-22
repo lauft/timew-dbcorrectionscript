c502f3f170a632f79a839a4cd5a5fe9f0844cecd
Thomas Lauf <thomas.lauf@tngtech.com>
2018-01-22 14:25:24 +0100

    Add version check

--- timew-dbcorrection.py
+++ timew-dbcorrection.py
@@ -1,8 +1,21 @@
+from __future__ import print_function
+
+
+def get_version():
+    return '0.0.0'
+
+
 if __name__ == "__main__":
     # check version
+    version_array = get_version().split(".")
+    major_version = int(version_array[0])
+    minor_version = int(version_array[1])
+
+    if major_version <= 1 and minor_version == 0:
+        print("This script requires TimeWarrior version 1.1.0 or larger!")
+        exit(1)
 
     # show disclaimer
-
     # export database
 
     # purge database
