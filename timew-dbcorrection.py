25a2f65ef58a3ee836bc7566b2e6611c7374a797
Thomas Lauf <thomas.lauf@tngtech.com>
2018-01-22 21:28:30 +0100

    First version of disclaimer

--- timew-dbcorrection.py
+++ timew-dbcorrection.py
@@ -16,6 +16,22 @@ if __name__ == "__main__":
         exit(1)
 
     # show disclaimer
+    print("""
+TIMEWARRIOR DATABASE CORRECTION SCRIPT
+
+Use this script when you have used
+- TimeWarrior before version 1.1.1
+- exclusions
+
+If none of the above applies, you probably do not need to run this script.
+
+This script will export your stored intervals, purge your database and re-enter
+your data such that exclusions will be applied and properly written to the
+database. For further information on this see http://timewarrior.net/some/url.
+
+!!! YOU ARE STRONGLY ADVISED TO BACKUP YOUR TIMEWARRIOR DATABASE BEFORE PROCEEDING !!!
+
+""")
     # export database
 
     # purge database
